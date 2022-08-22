ad5ce19af9fba2583437fb759adb31bc5ee9fd0d
Vandanpatel5980 <vandanpatel5980@gmail.com>
2022-08-22 21:18:04 +0530

    Pr1

---
+++ src/main/kotlin/mian.kt
@@ -0,0 +1,21 @@
+fun main(args : Array<String>){
+    var int:Int=15000
+    var doub:Double=45.0
+    var ft:Float=362.0F
+    var lg:Long=20012011130
+    var srt:Short=1
+    var byte:Byte=12
+    var char:Char='D'
+    var bool:Boolean=true
+    var string:String="Vandan Patel"
+
+    println("Integer: $int")
+    println("Double: $doub")
+    println("Float: $ft")
+    println("Long: $lg")
+    println("Short: $srt")
+    println("Byte: $byte")
+    println("Char: $char")
+    println("Boolean: $bool")
+    println("String: $string")
+}
